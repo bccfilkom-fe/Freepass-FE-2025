b2034aa321de560248903cdf6fb02dc64d112f35
ahargunyllib <billy.bpm03@gmail.com>
2025-01-26 13:00:48 +0700

    feat: restructure landing page with new header and hero section components

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/features/landing/components/header";
+import HeroSection from "@/features/landing/sections/hero-section";
+
+export default function Page() {
+	return (
+		<>
+			<Header />
+			<HeroSection />
+		</>
+	);
+}
