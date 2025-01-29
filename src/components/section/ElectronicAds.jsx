daf11ed5b186914841fd4b96d2051b85decfba4e
syaquiii <msyauqioqi@gmail.com>
2025-01-29 07:20:34 +0700

    test

--- src/components/section/ElectronicAds.jsx
+++ src/components/section/ElectronicAds.jsx
@@ -6,7 +6,7 @@ import Tv from "../../../public/Tv.png";
 const ElectronicAds = () => {
   return (
     <div className=" w-full px-10 h-60  rounded-xl flex flex-row-reverse items-center">
-      <Image className="aspect-square w-2/5 md:w-1/3 h-fit" src={Tv} alt="" />
+      <Image className="aspect-square w-2/5 md:w-1/3 h-fit" src={Tv} alt="Tv" />
       <div className=" flex flex-col gap-2 md:gap-4">
         <h5>Timeless Innovation</h5>
         <h2 className="md:text-4xl text-xl font-bold">
