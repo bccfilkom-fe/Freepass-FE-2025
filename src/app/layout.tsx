51cd542182d1d564765df07b2221e2aef87e762a
ahargunyllib <billy.bpm03@gmail.com>
2025-01-29 22:17:59 +0700

    feat: add metadata, robots, and sitemap for improved SEO and site structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,23 @@
 import Provider from "@/shared/components/providers";
 import { fontsVariables } from "@/shared/lib/fonts";
 import "@/shared/styles/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "BCC Conference",
+	description: "BCC Conference, part of Freepass challenge by BCC FILKOM UB",
+	metadataBase: new URL("https://freepass-fe-bcc-2025.ahargunyllib.tech"),
+	icons: "/icon.png",
+	openGraph: {
+		title: "BCC Conference",
+		description: "BCC Conference, part of Freepass challenge by BCC FILKOM UB",
+	},
+	twitter: {
+		title: "BCC Conference",
+		description: "BCC Conference, part of Freepass challenge by BCC FILKOM UB",
+		card: "summary_large_image",
+	},
+};
 
 export default function RootLayout({
 	children,
