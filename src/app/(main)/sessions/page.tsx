4c90d62f83178b58af611d5d817979946903468e
ahargunyllib <billy.bpm03@gmail.com>
2025-01-29 19:08:33 +0700

    feat(session-list): refactor layout to use flexbox for improved responsiveness

--- src/app/(main)/sessions/page.tsx
+++ src/app/(main)/sessions/page.tsx
@@ -2,7 +2,7 @@ import SessionList from "../../../features/session/componenets/session-list";
 
 export default function Page() {
 	return (
-		<main className=" h-full w-full flex flex-col gap-4">
+		<main className=" h-full w-full flex flex-col gap-4 items-center">
 			<h1 className="text-5xl md:text-7xl tracking-tighter text-center">
 				Join a Session
 			</h1>
--- src/features/session/componenets/session-list.tsx
+++ src/features/session/componenets/session-list.tsx
@@ -21,11 +21,8 @@ export default function SessionList() {
 
 	if (isLoading || !data) {
 		return (
-			<div className="h-5/6 grid grid-cols-6 grid-rows-5 gap-x-4">
-				<div className="border-2 rounded-lg col-span-2 row-span-5" />
+			<div className="h-5/6 flex flex-col gap-4 w-5/6">
 				<div className="col-span-4 row-span-5 overflow-y-auto gap-y-4 grid w-full">
-					<Skeleton className="w-full h-40 border-2 rounded-lg flex-shrink-0" />
-					<Skeleton className="w-full h-40 border-2 rounded-lg flex-shrink-0" />
 					<Skeleton className="w-full h-40 border-2 rounded-lg flex-shrink-0" />
 					<Skeleton className="w-full h-40 border-2 rounded-lg flex-shrink-0" />
 					<Skeleton className="w-full h-40 border-2 rounded-lg flex-shrink-0" />
@@ -35,65 +32,7 @@ export default function SessionList() {
 	}
 
 	return (
-		<div className="h-5/6  grid grid-cols-6 grid-rows-5 gap-x-4">
-			<div className="border-2 rounded-lg col-span-2 row-span-5 p-4 space-y-4 overflow-y-auto">
-				<div className="space-y-2">
-					<Label>Search</Label>
-					<Input placeholder="Search by title..." />
-				</div>
-				<div className="space-y-2">
-					<Label>Tags</Label>
-					{Object.keys(SessionTagMap).map((tag) => (
-						<div
-							key={tag}
-							className="flex flex-row items-start space-x-3 space-y-0"
-						>
-							<Checkbox
-							// checked={prop.field.value?.includes(tag)}
-							// onCheckedChange={(checked) => {
-							// 	return checked
-							// 		? prop.field.onChange([...(prop.field.value || []), tag])
-							// 		: prop.field.onChange(
-							// 				prop.field.value?.filter((value) => value !== tag),
-							// 			);
-							// }}
-							/>
-							<span className="text-sm font-normal">
-								{SessionTagMap[tag as keyof typeof SessionTagMap]}
-							</span>
-						</div>
-					))}
-				</div>
-				<div className="space-y-2">
-					<Label>Place</Label>
-					<RadioGroup defaultValue="all">
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="all" id="r1" />
-							<Label htmlFor="r1">All</Label>
-						</div>
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="offline" id="r2" />
-							<Label htmlFor="r2">Offline</Label>
-						</div>
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="online" id="r3" />
-							<Label htmlFor="r3">Online</Label>
-						</div>
-					</RadioGroup>
-				</div>
-				<div className="space-y-2">
-					<Label>Start Date</Label>
-					<DateField
-						granularity="minute"
-						hourCycle={12}
-						// onChange={(value) => {
-						// 	if (value) prop.field.onChange(new Date(value.toString()));
-						// }}
-					>
-						<DateInput />
-					</DateField>
-				</div>
-			</div>
+		<div className="h-5/6 flex flex-col gap-4 w-5/6">
 			<div className="col-span-4 row-span-5 overflow-y-auto gap-y-4 grid w-full">
 				{data.payload.sessions.length > 0 ? (
 					data.payload.sessions.map((session) => {
